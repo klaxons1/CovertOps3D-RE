72048e0b576ddbf8ea67b545afe2f3bb9b58e7df
klaxons1 <59658354+klaxons1@users.noreply.github.com>
2025-12-02 23:21:31 +0300

    merged + wav and mp3 support

--- CovertOps3d/src/AudioManager.java
+++ CovertOps3d/src/AudioManager.java
@@ -5,96 +5,165 @@ import javax.microedition.media.Player;
 import javax.microedition.media.control.VolumeControl;
 
 public final class AudioManager {
-   private int currentSoundIndex = 0;
-   private Vector soundPlayers = new Vector();
-   private VolumeControl volumeControl;
+    private int currentSoundIndex = -1;
+    private Vector soundPlayers = new Vector();
+    private VolumeControl volumeControl;
 
-   public final void loadSound(String var1) {
-      Player var2 = null;
+    /**
+     * Inner class to hold Player instance and its priority.
+     * Merged SoundPlayer into AudioManager to save space and reduce class count.
+     */
+    private static class SoundItem {
+        public Player player;
+        public int priority; // Formerly 'soundId'
 
-      try {
-         InputStream var3 = this.getClass().getResourceAsStream(var1);
-         int var4;
-         if ((var4 = var1.indexOf(".")) == -1) {
-            return;
-         }
+        public SoundItem(Player player, int priority) {
+            this.player = player;
+            this.priority = priority;
+        }
+    }
 
-         String var5;
-         if ((var5 = new String(var1.getBytes(), var4, var1.length() - var4)).compareTo(".mid") == 0) {
-            var2 = Manager.createPlayer(var3, "audio/midi");
-         } else {
-            if (var5.compareTo(".amr") != 0) {
-               return;
+    /**
+     * Loads an audio file from resources.
+     * Supported formats: .mid, .amr, .mp3, .wav
+     *
+     * @param resourcePath Path to the audio file (e.g., "/music.mp3").
+     */
+    public final void loadSound(String resourcePath) {
+        Player player = null;
+
+        try {
+            InputStream inputStream = getClass().getResourceAsStream(resourcePath);
+            int dotIndex = resourcePath.lastIndexOf('.');
+
+            if (dotIndex == -1) {
+                System.err.println("Error: No extension found in file name: " + resourcePath);
+                return;
             }
 
-            var2 = Manager.createPlayer(var3, "audio/amr");
-         }
+            // Detect format
+            String extension = resourcePath.substring(dotIndex).toLowerCase();
+            String contentType = null;
 
-         var2.realize();
-         var2.prefetch();
-         var2.setLoopCount(1);
-         this.volumeControl = (VolumeControl)var2.getControl("VolumeControl");
-      } catch (Exception var6) {
-         System.err.println(var6.getMessage());
-         var6.printStackTrace();
-         if (var2 != null) {
-            var2.close();
-         }
-      }
-
-      this.soundPlayers.addElement(new SoundPlayer(this, var2, 0));
-   }
-
-   public final void playSound(int var1, int var2, int var3) {
-      try {
-         SoundPlayer var4;
-         Player var5;
-         if (this.currentSoundIndex > -1 && (var5 = (var4 = (SoundPlayer)this.soundPlayers.elementAt(this.currentSoundIndex)).player) != null) {
-            SoundPlayer var6;
-            SoundPlayer.setSoundId(var6 = (SoundPlayer)this.soundPlayers.elementAt(var1), var3);
-            if (var5.getState() == 400) {
-               if (var4.soundId > var6.soundId) {
-                  return;
-               }
-
-               var5.stop();
-               var5.setMediaTime(0L);
+            if (extension.equals(".mid")) {
+                contentType = "audio/midi";
+            } else if (extension.equals(".amr")) {
+                contentType = "audio/amr";
+            } else if (extension.equals(".mp3")) {
+                contentType = "audio/mpeg";
+            } else if (extension.equals(".wav")) {
+                contentType = "audio/x-wav";
+            } else {
+                System.err.println("Unsupported format: " + extension);
+                return;
             }
 
-            Player var7;
-            if ((var7 = var6.player) != null) {
-               var7.setLoopCount(var2);
-               var7.start();
-               this.currentSoundIndex = var1;
+            // Create and prepare player
+            player = Manager.createPlayer(inputStream, contentType);
+            player.realize();
+            player.prefetch();
+            player.setLoopCount(1);
+
+            // Grab volume control (usually global for the device mixer)
+            if (player.getControl("VolumeControl") != null) {
+                this.volumeControl = (VolumeControl) player.getControl("VolumeControl");
             }
-         }
 
-         System.err.print("Exec player:");
-         System.err.println(this.soundPlayers);
-      } catch (Exception var8) {
-         System.err.println(var8.getMessage());
-         var8.printStackTrace();
-      }
-   }
+        } catch (Exception e) {
+            System.err.println("Error loading sound: " + e.getMessage());
+            e.printStackTrace();
+            if (player != null) {
+                player.close();
+                player = null;
+            }
+        }
 
-   public final void stopCurrentSound() {
-      try {
-         Player var2;
-         if (this.currentSoundIndex > -1 && (var2 = ((SoundPlayer)this.soundPlayers.elementAt(this.currentSoundIndex)).player) != null) {
-            var2.stop();
-            var2.setMediaTime(0L);
-         }
+        // Add new sound wrapper to the vector
+        this.soundPlayers.addElement(new SoundItem(player, 0));
+    }
 
-      } catch (Exception var3) {
-         System.err.println(var3.getMessage());
-         var3.printStackTrace();
-      }
-   }
+    /**
+     * Plays a specific sound.
+     *
+     * @param index     The index of the sound in the loaded vector.
+     * @param loopCount Number of loops (-1 for infinite).
+     * @param priority  Priority of this playback (higher value = more important).
+     */
+    public final void playSound(int index, int loopCount, int priority) {
+        try {
+            SoundItem currentItem = null;
+            Player currentPlayer = null;
 
-   public final void setVolume(int var1) {
-      if (this.volumeControl != null) {
-         this.volumeControl.setLevel(var1);
-      }
+            // 1. Get the currently playing sound (if any)
+            if (this.currentSoundIndex > -1 && this.currentSoundIndex < this.soundPlayers.size()) {
+                currentItem = (SoundItem) this.soundPlayers.elementAt(this.currentSoundIndex);
+                currentPlayer = currentItem.player;
+            }
 
-   }
+            // Check index bounds
+            if (index < 0 || index >= this.soundPlayers.size()) return;
+
+            // 2. Get the new sound item
+            SoundItem newItem = (SoundItem) this.soundPlayers.elementAt(index);
+
+            // Update the priority for this specific playback call
+            newItem.priority = priority;
+
+            // 3. Handle interruption logic
+            if (currentPlayer != null && currentPlayer.getState() == Player.STARTED) {
+                // If current sound is more important than the new one, do nothing (exit)
+                if (currentItem.priority > newItem.priority) {
+                    return;
+                }
+
+                // Stop current sound
+                currentPlayer.stop();
+                currentPlayer.setMediaTime(0L);
+            }
+
+            // 4. Start the new sound
+            Player newPlayer = newItem.player;
+            if (newPlayer != null) {
+                newPlayer.setLoopCount(loopCount);
+                newPlayer.start();
+                this.currentSoundIndex = index;
+            }
+
+            // Debug output
+            // System.out.println("Playing sound index: " + index);
+
+        } catch (Exception e) {
+            System.err.println("Error playing sound: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Stops the currently active sound.
+     */
+    public final void stopCurrentSound() {
+        try {
+            if (this.currentSoundIndex > -1 && this.currentSoundIndex < this.soundPlayers.size()) {
+                SoundItem item = (SoundItem) this.soundPlayers.elementAt(this.currentSoundIndex);
+                Player player = item.player;
+                if (player != null) {
+                    player.stop();
+                    player.setMediaTime(0L);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error stopping sound: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sets the volume for the audio manager.
+     * @param level Volume level (0-100)
+     */
+    public final void setVolume(int level) {
+        if (this.volumeControl != null) {
+            this.volumeControl.setLevel(level);
+        }
+    }
 }
